8952e3df905f74e4c3f0bbc5cd418816b8239530
Ihor Skohariev <skogrv@gmail.com>
2025-04-01 14:47:24 +0300

    fix: specify version correctly

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-APP_VERSION = '0.0.1'
+APP_VERSION = 'v0.0.2'
 
 
-def print_version(): 
+def print_version():
     print(f"{APP_VERSION}")
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     print_version()
